4a1727c3b7df6e645bfaec3e749b79e6fb2c5ea8
PJZ9n <pjz9n@yahoo.co.jp>
2020-01-28 05:21:57 +0900

    パーミッション設定のサポート

--- src/jojoe77777/FormAPI/Form.php
+++ src/jojoe77777/FormAPI/Form.php
@@ -6,6 +6,7 @@ namespace jojoe77777\FormAPI;
 
 use pocketmine\form\Form as IForm;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 abstract class Form implements IForm{
 
@@ -13,6 +14,10 @@ abstract class Form implements IForm{
     protected $data = [];
     /** @var callable|null */
     private $callable;
+    /** @var string|null */
+    private $permission;
+    /** @var string|null */
+    private $permissionMessage;
 
     /**
      * @param callable|null $callable
@@ -39,7 +44,55 @@ abstract class Form implements IForm{
         $this->callable = $callable;
     }
 
+    public function getPermission() : ?string {
+        return $this->permission;
+    }
+
+    public function setPermission(?string $permission) : void {
+        $this->permission = $permission;
+    }
+
+    public function getPermissionMessage() : ?string {
+        return $this->permissionMessage;
+    }
+
+    public function setPermissionMessage(?string $permissionMessage) : void {
+        $this->permissionMessage = $permissionMessage;
+    }
+
+    public function testPermission(Player $target): bool {
+        if ($this->testPermissionSilent($target)) {
+            return true;
+        }
+        
+        if ($this->permissionMessage === null) {
+            $target->sendMessage(TextFormat::RED . "You do not have permission to use this form");
+        } else if ($this->permissionMessage !== "") {
+            $target->sendMessage(str_replace("<permission>", $this->permission, $this->permissionMessage));
+        }
+        
+        return false;
+    }
+
+    public function testPermissionSilent(Player $target): bool
+    {
+        if ($this->permission === null || $this->permission === "") {
+            return true;
+        }
+        
+        foreach (explode(";", $this->permission) as $permission) {
+            if ($target->hasPermission($permission)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
     public function handleResponse(Player $player, $data) : void {
+        if (!$this->testPermission($player)) {
+            return;
+        }
         $this->processData($data);
         $callable = $this->getCallable();
         if($callable !== null) {
